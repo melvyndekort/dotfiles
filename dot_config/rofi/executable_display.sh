29edbf91cece38d6e82990b05bb3b626f588ffb8
Melvyn de Kort <melvyn@mdekort.nl>
2020-09-14 16:03:51 +0200

    Fix DPI problem with alacritty

--- dot_config/rofi/executable_display.sh
+++ dot_config/rofi/executable_display.sh
@@ -21,7 +21,7 @@ fi
 case $(echo "$1" | cut -d' ' -f1) in
   *laptop*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --off \
@@ -31,21 +31,21 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *desktop-1*)
     xrandr \
-      --output DP-1 --primary --mode 1680x1050 --pos 0x0 \
-      --output DVI-I-1 --mode 1680x1050 --pos 1680x0 \
+      --output DP-1 --primary --dpi 96 --mode 1680x1050 --pos 0x0 \
+      --output DVI-I-1 --dpi 96 --mode 1680x1050 --pos 1680x0 \
       --output DP-0 --off \
       --output DVI-I-0 --off ;;
 
   *desktop-2*)
     xrandr \
-      --output DP-1 --primary --mode 1680x1050 --pos 0x0 \
+      --output DP-1 --primary --dpi 96 --mode 1680x1050 --pos 0x0 \
       --output DVI-I-1 --off \
       --output DP-0 --off \
       --output DVI-I-0 --off ;;
 
   *dock-clone*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
       --output DVI-I-1-1 --same-as eDP-1 \
       --output DVI-I-2-2 --same-as eDP-1 \
       --output DP-1 --off \
@@ -55,9 +55,9 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *dock-expand-1*)
     xrandr \
-      --output DVI-I-1-1 --primary --mode 1920x1080 --pos 0x0 \
-      --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 \
-      --output eDP-1 --mode 1920x1080 --pos 3840x504 \
+      --output DVI-I-1-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
+      --output DVI-I-2-2 --dpi 96 --mode 1920x1080 --pos 1920x0 \
+      --output eDP-1 --dpi 96 --mode 1920x1080 --pos 3840x504 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --off \
@@ -65,9 +65,9 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *dock-expand-2*)
     xrandr \
-      --output DVI-I-2-2 --primary --mode 1920x1080 --pos 0x0 \
-      --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 \
-      --output eDP-1 --mode 1920x1080 --pos 3840x504 \
+      --output DVI-I-2-2 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
+      --output DVI-I-1-1 --dpi 96 --mode 1920x1080 --pos 1920x0 \
+      --output eDP-1 --dpi 96 --mode 1920x1080 --pos 3840x504 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --off \
@@ -75,9 +75,9 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *dock-expand-3*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x504 \
-      --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 \
-      --output DVI-I-2-2 --mode 1920x1080 --pos 3840x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x504 \
+      --output DVI-I-1-1 --dpi 96 --mode 1920x1080 --pos 1920x0 \
+      --output DVI-I-2-2 --dpi 96 --mode 1920x1080 --pos 3840x0 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --off \
@@ -85,9 +85,9 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *dock-expand-4*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x504 \
-      --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 \
-      --output DVI-I-1-1 --mode 1920x1080 --pos 3840x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x504 \
+      --output DVI-I-2-2 --dpi 96 --mode 1920x1080 --pos 1920x0 \
+      --output DVI-I-1-1 --dpi 96 --mode 1920x1080 --pos 3840x0 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --off \
@@ -95,7 +95,7 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *hdmi-clone*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output HDMI-1 --same-as eDP-1 \
@@ -105,8 +105,8 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *hdmi-expand-1*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x0 \
-      --output HDMI-1 --mode 1920x1080 --pos 1920x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
+      --output HDMI-1 --dpi 96 --mode 1920x1080 --pos 1920x0 \
       --output HDMI-2 --off \
       --output DP-1 --off \
       --output DP-2 --off \
@@ -115,9 +115,9 @@ case $(echo "$1" | cut -d' ' -f1) in
 
   *hdmi-expand-2*)
     xrandr \
-      --output eDP-1 --primary --mode 1920x1080 --pos 0x0 \
-      --output HDMI-1 --mode 1920x1080 --pos 1920x0 \
-      --output HDMI-2 --mode 1920x1080 --pos 3840x0 \
+      --output eDP-1 --primary --dpi 96 --mode 1920x1080 --pos 0x0 \
+      --output HDMI-1 --dpi 96 --mode 1920x1080 --pos 1920x0 \
+      --output HDMI-2 --dpi 96 --mode 1920x1080 --pos 3840x0 \
       --output DP-1 --off \
       --output DP-2 --off \
       --output DVI-I-1-1 --off \
