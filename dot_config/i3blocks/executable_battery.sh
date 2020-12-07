d0e447fd77817ece01930bbb18a25cf15c34d41e
Melvyn de Kort <melvyn@mdekort.nl>
2020-12-07 11:24:53 +0100

    Battery improvement

--- dot_config/i3blocks/executable_battery.sh
+++ dot_config/i3blocks/executable_battery.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 printline() {
-  echo "<span font_family=\"Hack Nerd Font Mono\" size=\"medium\">$1</span> $2%"
-  echo "<span font_family=\"Hack Nerd Font Mono\" size=\"medium\">$1</span> $2%"
+  echo "<span font_family=\"Hack Nerd Font Mono\" size=\"medium\">$1</span>$2"
+  echo "<span font_family=\"Hack Nerd Font Mono\" size=\"medium\">$1</span>$2"
 }
 
 case $BLOCK_BUTTON in
@@ -23,51 +23,51 @@ STATUS="$(cat /sys/class/power_supply/${INSTANCE}/status)"
 CAPACITY="$(cat /sys/class/power_supply/${INSTANCE}/capacity)"
 
 if [ "$STATUS" = "Charging" ]; then
-  if [ "$CAPACITY" -le "10" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "20" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "30" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "40" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "50" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "60" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "70" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "80" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "90" ]; then
-    printline "" $CAPACITY
+  if [ "" ${CAPACITY}%"" -le "10" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "20" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "30" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "40" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "50" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "60" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "70" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "80" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "90" ]; then
+    printline "" " ${CAPACITY}%"
   else
-    printline "" $CAPACITY
+    printline "" " ${CAPACITY}%"
   fi
 else
-  if [ "$CAPACITY" -le "10" ]; then
-    printline "" $CAPACITY
+  if [ "" ${CAPACITY}%"" -le "10" ]; then
+    printline "" " ${CAPACITY}%"
     exit 33
-  elif [ "$CAPACITY" -le "20" ]; then
-    printline "" $CAPACITY
+  elif [ "" ${CAPACITY}%"" -le "20" ]; then
+    printline "" " ${CAPACITY}%"
     echo "#ff5555"
-  elif [ "$CAPACITY" -le "30" ]; then
-    printline "" $CAPACITY
+  elif [ "" ${CAPACITY}%"" -le "30" ]; then
+    printline "" " ${CAPACITY}%"
     echo "#ffb86c"
-  elif [ "$CAPACITY" -le "40" ]; then
-    printline "" $CAPACITY
+  elif [ "" ${CAPACITY}%"" -le "40" ]; then
+    printline "" " ${CAPACITY}%"
     echo "#f1fa8c"
-  elif [ "$CAPACITY" -le "50" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "60" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "70" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "80" ]; then
-    printline "" $CAPACITY
-  elif [ "$CAPACITY" -le "90" ]; then
-    printline "" $CAPACITY
+  elif [ "" ${CAPACITY}%"" -le "50" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "60" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "70" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "80" ]; then
+    printline "" " ${CAPACITY}%"
+  elif [ "" ${CAPACITY}%"" -le "90" ]; then
+    printline "" " ${CAPACITY}%"
   else
-    printline "" $CAPACITY
+    printline "" " ${CAPACITY}%"
   fi
 fi
