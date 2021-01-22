f172c695af501365030003f99fe5797a7122d1b0
Melvyn de Kort <m.de.kort@portbase.com>
2021-01-22 13:52:21 +0100

    Dunst pause block clickable

--- dot_config/i3blocks/executable_notifications.sh
+++ dot_config/i3blocks/executable_notifications.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+case $BLOCK_BUTTON in
+  1) notify-send "DUNST_COMMAND_TOGGLE" ;;
+	2) notify-send " Notification module" "\- Click to pause/unpause" ;;
+esac
+
 printf "\n\n"
 
 dunstctl is-paused | grep -q '^true$'
