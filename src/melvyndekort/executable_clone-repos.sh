9172bd34b864bc95baedf1c03cbf9eb4448d1bab
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-23 11:44:45 +0200

    Singular updated script

--- src/melvyndekort/executable_clone-repos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for i in $(gh repo list melvyndekort --json name -q '.[].name'); do
-  [ ! -d "$i" ] && echo "Cloning $i" && gh repo clone $i -- --quiet
-done
-
--- src/melvyndekort/executable_update-repos.sh
+++ src/melvyndekort/executable_update-repos.sh
@@ -1,16 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-for REPO in ${PWD}/*/; do
-  cd $REPO
-  echo Processing $REPO
+BASEDIR=$PWD
+REPOLIST=$(gh repo list melvyndekort --json name -q '.[].name' | sort)
+SIZE=$(echo $REPOLIST | wc -w)
+COUNT=1
 
-  ## Set correct e-mail address
-  git config user.email 'melvyn@mdekort.nl'
-
-  ## Update repo
-  git branch --no-color --show-current | egrep -q "main|gh-pages"
-  if [ $? -eq 0 ]; then
-    git pull -q
+for REPO in $REPOLIST; do
+  if [ -d "$REPO" ]; then
+    echo "Updating $REPO (${COUNT}/${SIZE})"
+    cd $REPO
+    git config user.email 'melvyn@mdekort.nl'
+    ( git branch --no-color --show-current | grep -q main ) && git pull -q
+    cd $BASEDIR
+  else
+    echo "Cloning $REPO (${COUNT}/${SIZE})"
+    gh repo clone $REPO -- --quiet
+    cd $REPO
+    git config user.email 'melvyn@mdekort.nl'
+    cd $BASEDIR
   fi
+  COUNT=$((COUNT + 1))
 done
+
+DIRLIST=$(ls -d */ | xargs | tr -d '/' | sort)
+for REPO in $REPOLIST; do
+  DIRLIST=$(echo ${DIRLIST/$REPO/})
+done
+
+[ "" != "$DIRLIST" ] && echo "Repositories which aren't managed anymore: $DIRLIST"
 
