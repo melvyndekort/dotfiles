b5d0478c91d1897704dab0a6f8d46b573abbfd74
Melvyn de Kort <melvyn@mdekort.nl>
2022-11-18 14:11:24 +0100

    Don't expire calendar notifications

--- dot_config/i3blocks/executable_datetime.sh
+++ dot_config/i3blocks/executable_datetime.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 case $BLOCK_BUTTON in
-  1) notify-send "Date" "$(cal)" ;;
+  1) notify-send -t 0 "Date" "$(cal)" ;;
   2) notify-send "Datetime module" "\- Left click to show the calendar of this month
 - Right click to show the calendar for this year" ;;
-  3) notify-send "Date" "$(cal -y)" ;;
+  3) notify-send -t 0 "Date" "$(cal -y)" ;;
 esac
 
 echo "<span size='16pt'></span> <span rise='2pt'>$(date '+%Y-%m-%d %H:%M:%S')</span>"
