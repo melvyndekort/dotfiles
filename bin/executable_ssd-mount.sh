382f50882719a9e051ad56d200401ecfdae7fe66
Melvyn de Kort <melvyn@mdekort.nl>
2022-12-28 17:15:48 +0100

    Add script to mount SSD

---
+++ bin/executable_ssd-mount.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'Please input the LUKS passphrase:'
+udisksctl unlock -b /dev/disk/by-partlabel/SSDCrypt
+udisksctl mount -b /dev/disk/by-label/SSDData
+udisksctl mount -b /dev/disk/by-label/SSDCrypt
+ranger
+udisksctl unmount -b /dev/disk/by-label/SSDData
+udisksctl unmount -b /dev/disk/by-label/SSDCrypt
+udisksctl lock -b /dev/disk/by-partlabel/SSDCrypt
+
