0c309130b6b7f0efc928f75ef770a77be20df29e
Melvyn de Kort <melvyn@mdekort.nl>
2024-11-03 20:05:09 +0100

    First iteration of custom neovim setup

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,32 @@
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+  vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+  vim.o.runtimepath = vim.o.runtimepath
+  vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
+-- vim-plug section
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+Plug 'Mofiqul/dracula.nvim'
+Plug 'jeffkreeftmeijer/neovim-sensible'
+vim.call('plug#end')
+
+vim.cmd[[colorscheme dracula]]
+
+-- Basic settings
+vim.o.number = true -- Enable line numbers
+vim.o.relativenumber = true -- Enable relative line numbers
+vim.o.tabstop = 2 -- Number of spaces a tab represents
+vim.o.shiftwidth = 2 -- Number of spaces for each indentation
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.smartindent = true -- Automatically indent new lines
+vim.o.wrap = false -- Disable line wrapping
+vim.o.cursorline = true -- Highlight the current line
+vim.o.termguicolors = true -- Enable 24-bit RGB colors
+
+-- Syntax highlighting and filetype plugins
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
