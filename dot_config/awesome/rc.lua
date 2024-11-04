32c13f98d0968fe712977fd1a0baeca62f6fa2c1
Melvyn de Kort <melvyn@mdekort.nl>
2024-11-04 18:58:51 +0100

    Add applications to their own tag by default

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -411,8 +411,18 @@ awful.rules.rules = {
   { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } },
 
   -- Set Firefox to always map on the tag named "2" on screen 1.
-  -- { rule = { class = "Firefox" },
-  --   properties = { screen = 1, tag = "2" } },
+  { rule = { class = "Firefox" },
+    properties = { screen = 1, tag = "2" } },
+  { rule = { class = "Slack" },
+    properties = { screen = 1, tag = "3" } },
+  { rule = { class = "TelegramDesktop" },
+    properties = { screen = 1, tag = "4" } },
+  { rule = { class = "VSCodium" },
+    properties = { screen = 1, tag = "6" } },
+  { rule = { class = "Chromium" },
+    properties = { screen = 1, tag = "7" } },
+  { rule = { class = "Spotify" },
+    properties = { screen = 1, tag = "9" } },
 }
 -- }}}
 
