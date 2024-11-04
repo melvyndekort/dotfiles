aeef43527fde632e9321c464dd9bb86a91849211
Melvyn de Kort <melvyn@mdekort.nl>
2024-11-04 17:07:05 +0100

    More efficient code and fit instead of centered

--- dot_config/awesome/wallpaper.lua
+++ dot_config/awesome/wallpaper.lua
@@ -5,17 +5,12 @@ local colors = require("colors")
 -- Base directory for wallpapers
 local wallpaper_base_dir = "/home/melvyn/Sync/pictures/wallpapers/"
 
--- Function to get a list of files in the specified directory
-local function get_wallpaper_files(dir)
-  local files = {}
-  local p = io.popen('ls "' .. dir .. '"')
-  if p then
-    for file in p:lines() do
-      table.insert(files, file)
-    end
-    p:close()
-  end
-  return files
+-- Function to get a random wallpaper file from the specified directory using shuf
+local function get_random_wallpaper_file(dir)
+  local p = io.popen('find "' .. dir .. '" -type f | shuf -n 1')
+  local wallpaper = p:read("*line") -- Read the randomly selected wallpaper file
+  p:close()
+  return wallpaper
 end
 
 -- Function to set a random wallpaper based on screen resolution
@@ -24,17 +19,15 @@ function set_random_wallpaper(s)
   local resolution_folder = s.geometry.width .. "x" .. s.geometry.height
   local wallpaper_dir = wallpaper_base_dir .. resolution_folder .. "/"
 
-  -- Get the list of wallpapers in the matching resolution folder
-  local files = get_wallpaper_files(wallpaper_dir)
-  if #files > 0 then
-    -- Select a random wallpaper from the list
-    local wallpaper = wallpaper_dir .. files[math.random(#files)]
-    -- Set the wallpaper for the screen, centered
-    gears.wallpaper.centered(wallpaper, s, colors.background)  -- Background color around centered wallpaper
+  -- Get a random wallpaper in the matching resolution folder
+  local wallpaper = get_random_wallpaper_file(wallpaper_dir)
+  if wallpaper then
+    -- Set the wallpaper for the screen
+    gears.wallpaper.fit(wallpaper, s, colors.background)
   else
     -- Optionally, handle the case when no wallpapers are found for the resolution
     gears.debug.print_warning("No wallpapers found in " .. wallpaper_dir)
-    gears.wallpaper.centered(wallpaper_base_dir .. "default.png", s, colors.background)
+    gears.wallpaper.fit(wallpaper_base_dir .. "default.png", s, colors.background)
   end
 end
 
