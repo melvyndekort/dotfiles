4a478061d0657077ee9e77ea62534989b224e61c
Melvyn de Kort <melvyn@mdekort.nl>
2024-11-20 17:59:38 +0100

    Multiple improvements for awesome wm

--- dot_config/awesome/widgets/load.lua
+++ dot_config/awesome/widgets/load.lua
@@ -4,57 +4,54 @@ local wibox = require("wibox")
 local watch = require("awful.widget.watch")
 local colors = require("colors")
 
-local load_widget = {}
+local mywidget = {}
 
-local function worker(user_args)
-    local args = user_args or {}
+local function worker()
+  mywidget = wibox.widget {
+    background_color = colors.current,
+    border_color = colors.background,
+    color = colors.pink,
+    min_value = 0,
+    step_width = 3,
+    step_spacing = 2,
+    forced_width = 70,
+    widget = wibox.widget.graph
+  }
 
-    load_widget = wibox.widget {
-        background_color = colors.background,
-        border_color = colors.background,
-        color = colors.pink,
-        min_value = 0,
-        step_width = 3,
-        step_spacing = 2,
-        forced_height = 24,
-        forced_width = 80,
-        widget = wibox.widget.graph
-    }
+  local padded_widget = wibox.container.margin(mywidget, 14, 0, 2, 2)
 
-    local padded_widget = wibox.container.margin(load_widget, 10, 0, 0, 0)
+  awful.spawn.easy_async_with_shell("nproc", function(stdout, _, _, _)
+    mywidget.max_value = tonumber(stdout)
+  end)
 
-    awful.spawn.easy_async_with_shell("nproc", function(stdout, _, _, _)
-        load_widget.max_value = tonumber(stdout)
-    end)
+  local function update_widget(widget, stdout)
+    local avg1min,avg5min,avg15min = string.match(stdout, "^(%S+)%s+(%S+)%s+(%S+)")
+    widget:add_value(tonumber(avg1min))
+  end
 
-    local function update_widget(widget, stdout)
-        local avg1min,avg5min,avg15min = string.match(stdout, "^(%S+)%s+(%S+)%s+(%S+)")
-        widget:add_value(tonumber(avg1min))
-    end
+  watch("cat /proc/loadavg", 2, update_widget, mywidget)
 
-    watch("cat /proc/loadavg", 2, update_widget, load_widget)
-
-    load_widget:buttons(
-        awful.util.table.join(
-            awful.button({}, 1, function()
-                awful.spawn("kitty --single-instance -e btop")
-            end)
-        )
+  mywidget:buttons(
+    awful.util.table.join(
+      awful.button({}, 1, function()
+        awful.spawn("kitty --single-instance -e btop")
+      end)
     )
+  )
 
-    local tooltip = awful.tooltip {
-        objects = { load_widget }
-    }
+  local tooltip = awful.tooltip {
+    objects = { mywidget }
+  }
 
-    load_widget:connect_signal("mouse::enter", function()
-        local myfile = io.open("/proc/loadavg", "r")
-        tooltip.text = "  1m   5m  15m\n" .. myfile:read("*a")
-        myfile:close()
-    end)
+  mywidget:connect_signal("mouse::enter", function()
+    local myfile = io.open("/proc/loadavg", "r")
+    tooltip.text = "  1m   5m  15m\n" .. myfile:read("*a")
+    myfile:close()
+  end)
 
-    return padded_widget
+  return padded_widget
 end
 
-return setmetatable(load_widget, { __call = function(_, ...)
-    return worker(...)
+return setmetatable(mywidget, { __call = function(_, ...)
+  return worker(...)
 end })
