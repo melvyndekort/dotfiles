dbb2bc729f02e1b635b0a63c8fb832bca7546628
Melvyn de Kort <m.de.kort@portbase.com>
2024-12-13 18:38:27 +0100

    Several improvements and added flameshot config

--- bin/executable_ssd-mount.sh
+++ bin/executable_ssd-mount.sh
@@ -4,7 +4,7 @@ echo 'Please input the LUKS passphrase:'
 udisksctl unlock -b /dev/disk/by-partlabel/SSDCrypt
 udisksctl mount -b /dev/disk/by-label/SSDData
 udisksctl mount -b /dev/disk/by-label/SSDCrypt
-ranger
+yazi
 udisksctl unmount -b /dev/disk/by-label/SSDData
 udisksctl unmount -b /dev/disk/by-label/SSDCrypt
 udisksctl lock -b /dev/disk/by-partlabel/SSDCrypt
