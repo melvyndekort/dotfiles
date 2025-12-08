905f8d8495427495aaab16427dd92d3cbfb8b5dd
Melvyn de Kort <m.de.kort@portbase.com>
2025-12-08 16:04:34 +0100

    Add screenlayouts for workstations

---
+++ dot_config/screenlayout/private_executable_7VLB2W7.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 617x1080 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-2-5 --off --output DP-2-6 --off --output DP-2-6-6 --mode 1920x1080 --pos 0x0 --rotate normal
