cb7f9d2875024ca24aae5fdb9455041469f42e24
Melvyn de Kort <m.de.kort@portbase.com>
2025-12-16 15:21:25 +0100

    Improve polybar icons

--- dot_config/polybar/scripts/executable_notify.sh
+++ dot_config/polybar/scripts/executable_notify.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if dunstctl is-paused | grep -q true; then
+  # Notifications paused → red icon
   echo "%{F#FF5555}%{F-}"
 else
+  # Notifications enabled → green icon
   echo "%{F#50FA7B}%{F-}"
 fi
