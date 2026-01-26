fd8f04240a59b0f3fbcb68f7734cc4552d58620c
Melvyn de Kort <m.de.kort@portbase.com>
2026-01-26 12:24:16 +0100

    Switch to ghostty, and remove awesome wm config

--- dot_config/awesome/config/keybindings/mod.lua
+++
@@ -1,3 +0,0 @@
-local modkey = 'Mod4'
-
-return modkey
--- dot_config/polybar/scripts/executable_show-ips.sh
+++ dot_config/polybar/scripts/executable_show-ips.sh
@@ -6,7 +6,7 @@ echo "=== Default Gateway ==="
 ip route | grep default
 echo
 echo "=== DNS Status ==="
-resolvectl status
+resolvectl status --no-pager
 echo
 echo "=== Public IP ==="
 curl -s --max-time 3 http://ip-api.com/json/ | jq -r 'if .status == "success" then "IP: \(.query)\nLocation: \(.city), \(.country)\nISP: \(.isp)" else "Unable to fetch" end'
