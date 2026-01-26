d134d43711a1e99f8f3fa5035921b4dab4da9b2d
Melvyn de Kort <m.de.kort@portbase.com>
2026-01-26 16:23:30 +0100

    Revert switch from kitty to ghostty (keep awesome wm removal)

--- dot_config/polybar/scripts/executable_show-ips.sh
+++ dot_config/polybar/scripts/executable_show-ips.sh
@@ -6,7 +6,7 @@ echo "=== Default Gateway ==="
 ip route | grep default
 echo
 echo "=== DNS Status ==="
-resolvectl status --no-pager
+resolvectl status
 echo
 echo "=== Public IP ==="
 curl -s --max-time 3 http://ip-api.com/json/ | jq -r 'if .status == "success" then "IP: \(.query)\nLocation: \(.city), \(.country)\nISP: \(.isp)" else "Unable to fetch" end'
